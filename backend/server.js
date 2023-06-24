55802ed03ae404b6771f2ad8aa26812c33277b39
octa230 <mervynstunner@outlook.com>
2023-06-24 08:45:15 -0400

    deployment set up changes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const userRouter = require('./routes/userRouter')
 const productRouter = require('./routes/productRouter');
 const salesRouter = require('./routes/saleRouter')
 const { multipleSaleRoutes } = require('./routes/multipleSaleRouter');
+const path = require('path');
 
 const app = express();
 dotenv.config()
@@ -33,14 +34,26 @@ app.use('/api/wholesale', salesRouter)
 
 //routes
 
-app.get('/', asyncHandler(async(req, res)=> {
+/* app.get('/', asyncHandler(async(req, res)=> {
     res.send('home page')
 }))
-
+ */
 //errorMiddleware
 app.use(ErrorHandler)
 
+/* app.use(express.static(path.join(__dirname, '/../frontend/build')))
+app.get('*', (req, res)=>
+    res.sendFile(path.join(`${__dirname}, '/../frontend/build/index.html`))
+)  */
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res)=> res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    ))
+}else {
+    app.get('/', (req, res)=> res.send('please set to production mode'))
+}
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> {
 
